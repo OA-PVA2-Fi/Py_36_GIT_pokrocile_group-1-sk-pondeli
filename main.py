8157bc89de6599b4edb6f8bb023e83fdfa2b498d
Lukáš Lhotský <d23931@oa-opava.cz>
2025-03-17 15:10:42 +0100

    přidání schopnosti dělení

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from utils import secti
+from utils import deleni
 
 def main():
     x = 5
     y = 10
     vysledek = secti(x, y)
+    vysledek_deleni=deleni(x, y)
     print(f"Výsledek sčítání {x} a {y} je: {vysledek}")
+    print(f"Vysledek deleni {x} a {y} je: {vysledek_deleni}")
 
 if __name__ == '__main__':
     main()

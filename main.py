0377002df34fecc65c79363a619910c1acd810b9
Lukáš Kabaja <D23861@oa-opava.cz>
2025-03-17 15:25:50 +0100

    přidaná funkce číslo na procenta

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from utils import secti
+from utils import secti, prevod_na_procenta
+
 
 def main():
     x = 5
     y = 10
     vysledek = secti(x, y)
+    print('číslo', x,'je v procentech',prevod_na_procenta(x),'%')
+    print('číslo', y,'je v procentech',prevod_na_procenta(y),'%')
     print(f"Výsledek sčítání {x} a {y} je: {vysledek}")
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -21,3 +21,6 @@ def odecti(a, b):
 
 def deleni(a,b):
     return a / b
+
+def prevod_na_procenta (a):
+    return a * 100

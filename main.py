e27db9ed11aa07f6bad6719e40da8f4120ff6568
Kryštof Pernica <d23123@oa-opava.cz>
2025-03-17 15:33:20 +0100

    Tahleta funkce násobí dvě čísla mezi sebou.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from utils import secti
+from utils import nasobeni
 
 def main():
     x = 5
     y = 10
     vysledek = secti(x, y)
     print(f"Výsledek sčítání {x} a {y} je: {vysledek}")
-
+    print(f"Výsledek násobení {x} a {y} je: {nasobeni(x, y)}")
 if __name__ == '__main__':
     main()

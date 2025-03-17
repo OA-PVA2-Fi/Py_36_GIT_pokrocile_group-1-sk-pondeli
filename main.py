c466efbc8b270f604d142ca562c455dcd2e83a0f
Kateřina Jašurková <d23297@oa-opava.cz>
2025-03-17 15:14:02 +0100

    přídáno odčítání

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from utils import secti
+from utils import odecti
 
 def main():
     x = 5
     y = 10
     vysledek = secti(x, y)
+    vysledek_odcitani = odecti(x, y)
     print(f"Výsledek sčítání {x} a {y} je: {vysledek}")
+    print(f"Výsledek odčítání {x} - {y} je: {vysledek_odcitani}")
 
 if __name__ == '__main__':
     main()
